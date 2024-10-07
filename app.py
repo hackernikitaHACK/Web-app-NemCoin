489b1e3816f9b43fa49cc35936a2ba0d03105c3a
hackernikitaHACK <160152783+hackernikitaHACK@users.noreply.github.com>
2024-10-07 18:58:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, send_from_directory
 import sqlite3
 
 app = Flask(__name__)
